9f479c069041915b720779a8afe34fcf3d8f18e2
maxlp12 <maxlp12@gmail.com>
2024-01-31 02:37:34 -0500

    Fix animation bases marquee

--- script.js
+++ script.js
@@ -16,10 +16,40 @@ document.addEventListener('DOMContentLoaded', () => {
     drawText(document.querySelector('.glitch'), "Maxime\nLétourneau\nPelletier.");
     // Initial check
     handleScroll();
-
+    setMarquee(12);
 });
 
 
+function setMarquee(duration){
+    let marquee = document.querySelectorAll(".marquee");
+    for (let i = 0; i < marquee.length; i++){
+        let elements = marquee[i].querySelectorAll("ul");
+        let endPosition = 0;
+
+        for (let j = 0; j < elements.length; j++) {
+            endPosition += elements[j].offsetWidth;
+        }
+        console.log(endPosition);
+        for ( j = 0; j < elements.length; j++) {
+            let e = elements[j];
+            let direction = (e.className.includes('reverse')) ? "reverse" : "normal";
+            let start = (e.className.includes('reverse')) ? -e.offsetWidth : -e.offsetWidth;
+            let val = (duration/elements.length) * j;
+            e.animate([
+                // keyframes
+                { transform: `translateX(${start}px)` },
+                { transform: `translateX(${endPosition}px)` },
+              ], {
+                // timing options
+                duration: duration*1000,
+                iterations: Infinity,
+                delay : -val*1000,
+                direction : direction
+            });
+            console.log(e.getAnimations.pl);
+        }
+    }
+}
 function drawText(container, text){
     text = text.split('').reverse();
     console.log(container, text);
